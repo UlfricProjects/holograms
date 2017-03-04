13c9fcf64dcdd67873ee74e7168cc921c110d86a
Adam Harrison <ad@mharrison.us>
2017-03-04 10:03:32 -0500

    location api

--- src/main/java/com/ulfric/holograms/Hologram.java
+++ src/main/java/com/ulfric/holograms/Hologram.java
@@ -2,6 +2,7 @@ package com.ulfric.holograms;
 
 import java.util.List;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public interface Hologram {
@@ -9,6 +10,10 @@ public interface Hologram {
 	List<String> getLines();
 
 	void setLines(List<String> lines);
+
+	List<Location> getLocations();
+
+	void setLocations(List<Location> locations);
 
 	boolean isVisibleTo(Player player);
 
