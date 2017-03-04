5ab2638f7ed6bee45a04733ed8b08776795a63f9
Adam Harrison <ad@mharrison.us>
2017-03-04 10:01:56 -0500

    visibility api

--- src/main/java/com/ulfric/holograms/Hologram.java
+++ src/main/java/com/ulfric/holograms/Hologram.java
@@ -2,10 +2,18 @@ package com.ulfric.holograms;
 
 import java.util.List;
 
+import org.bukkit.entity.Player;
+
 public interface Hologram {
 
 	List<String> getLines();
 
 	void setLines(List<String> lines);
+
+	boolean isVisibleTo(Player player);
+
+	void showTo(Player player);
+
+	void hideFrom(Player player);
 
 }
